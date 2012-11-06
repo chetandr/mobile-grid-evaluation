e9bfc0b24fd5a249562986f96086391e7a36763b
Chetan Rane <crane1@sapient.com>
2012-11-06 16:06:06 +0530

    renamed the Fodler to Jquery

---
+++ jquery-mobile-jqgrid/js/main.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+
+    // Compute position values
+    $.each(positions, function(index, position) {
+        position.marketValue = position.lastTrade * position.quantity;
+        position.totalCost = position.pricePaid * position.quantity;
+        position.gain = position.marketValue - position.totalCost;
+        position.gainPercent = (position.gain/position.totalCost) * 100;
+    });
+
+    // Render table
+    var grid = $('#positions-table').jqGrid({
+    	"hoverrows":true,
+		"viewrecords":true,
+		"jsonReader":{"repeatitems":false,"subgrid":{"repeatitems":false}},
+		"gridview":true,
+		"loadonce":true,
+		"url":"datav.json",
+		"scroll":1,
+		"autowidth":true,
+		"autoheight":true,
+		"rowNum":20,		
+		"height":600,
+   	    "colNames":['Security','Symbol', 'Quantity', 'Last Trade','Market Value','Price Paid','Total Cost','Gain','Gain %'],
+   	    "colModel":[
+   		    {name:'security',index:'security', width:200},
+   		    {name:'symbol',index:'symbol', width:50},
+   		    {name:'quantity',index:'quantity', width:80},
+   		    {name:'lastTrade',index:'lastTrade', width:80, align:"right"},
+   		    {name:'marketValue',index:'marketValue', width:80, align:"right"},		
+   		    {name:'pricePaid',index:'pricePaid', width:80,align:"right"},		
+   		    {name:'totalCost',index:'totalCost', width:80, sortable:false},
+   		    {name:'gain',index:'gain', width:80, sortable:false},		
+   		    {name:'gainPercent',index:'gainPercent', width:80, sortable:false}		
+   	    ],
+        "datatype": 'local',
+        "data": positions,
+        "rowNum": 10000, /* this is a hack to workaround jqGrid bug */
+        onSelectRow: function(id){ 
+        		    		 $('#selected-position').html($('tr#'+id+' td:first-child').html());
+   }				      
+    });
+
+	
+    // Display window size on resize events
+    function displayWindowSize() {
+        var win = $(this);
+        $('.window-size').html("(" + win.width() + ", " + win.height() + ")");
+    }
+    $(window).resize(displayWindowSize);
+
+    // Show selection on click events
+    $('#positions-table tbody tr').click(function() {
+        var security = $(this).find('.security').html();
+        $('#selected-position').html(security);
+    });
+
+    // Fit table on resize events
+    var headerHeight = $('#positions-header').outerHeight(true);
+    var postionsSectionPadding = 30;
+    var selectionInfoHeight = $('#selected-position').outerHeight(true);
+    var layoutInfoHeight = $('.layout-info').outerHeight(true);
+    var fudgeFactor = 25; // don't know why this is needed!
+    var fixedSectionsHeight =
+        headerHeight + postionsSectionPadding + selectionInfoHeight + layoutInfoHeight + fudgeFactor;
+
+
+    function fitTable() {
+        var winWidth = $(this).width();
+        var winHeight = $(this).height();
+        $('#positions-table').setGridWidth(winWidth - 35, true);
+        $('#positions-table').setGridHeight(winHeight - fixedSectionsHeight);
+    }
+    $(window).resize(fitTable);
+
+    // Perform initial setup
+    displayWindowSize();
+    
+});
